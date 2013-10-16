2631a9a87281a86a84950959407df31eedb43985
helin16@gmail.com <helin16@gmail.com@ca1b4779-320e-dd91-a120-87a329077dbf>
2013-10-16 10:33:34 +0000

    added config file

--- updateConf.php
+++ updateConf.php
@@ -2,9 +2,9 @@
 
 <?php if(!isset($_REQUEST['confdata']) || trim($_REQUEST['confdata']) === '') {?>
 	<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
-	<textarea id="confdata" name="confdata">
+	<textarea id="confdata" name="confdata" style="width: 100%; height: 600px;">
 	<?php echo TemplateSelector::getConfiFile()?>
+	</textarea> 
 	<input type="submit" value="save" />
-	</textarea>
 	</form>
 <?php } else { TemplateSelector::writeToConfiFile($_REQUEST['confdata']); }?>
