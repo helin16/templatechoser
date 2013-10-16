af4039eb75e1b29976099553e8cdf916b00a7b15
helin16@gmail.com <helin16@gmail.com@ca1b4779-320e-dd91-a120-87a329077dbf>
2013-10-16 10:36:50 +0000

    added config file

--- updateConf.php
+++ updateConf.php
@@ -1,8 +1,8 @@
 <?php require_once dirname(__FILE__) . '/include/TemplateSelector.php'; ?>
 
-<?php if(isset($_REQUEST['confdata']) && trim($_REQUEST['confdata']) === '') {
+<?php if(isset($_REQUEST['confdata']) && trim($_REQUEST['confdata']) !== '') {
 	TemplateSelector::writeToConfiFile($_REQUEST['confdata']); 
-	echo 'saved';
+	echo 'saved!';
 }?>
 <form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
 <textarea id="confdata" name="confdata" style="width: 100%; height: 600px;"><?php echo TemplateSelector::getConfiFile()?></textarea> 
